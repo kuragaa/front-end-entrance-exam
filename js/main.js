17dbc07658ab5ed95f879013882619b4b6297d37
kuragaa <kenegesova.k@gmail.com>
2025-07-31 16:32:25 +0300

    complete responsive layout

--- js/main.js
+++ js/main.js
@@ -1,24 +1,15 @@
-import '../css/style.css'
-import javascriptLogo from '../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+document.addEventListener('DOMContentLoaded', () => {
+  const items = document.querySelectorAll('.language__item');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  items.forEach(item => {
+    const range = item.querySelector('input[type="range"]');
+    const fill = item.querySelector('.language__fill');
 
-setupCounter(document.querySelector('#counter'))
+    if (!range || !fill) return;
+
+    range.addEventListener('input', () => {
+      const val = range.value;
+      fill.style.width = `${val}%`;
+    });
+  });
+})
